30a7cab055586f274e151ad3f8018c9291173621
Divyaswor Makai <makaidivya@gmail.com>
2020-06-24 10:45:52 +0545

    1.4 completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -27,31 +27,39 @@ const Content=(props)=>{
 }
 
 const Total =(props)=>{
+  let total =0
+  props.part.forEach(element=>{
+    total+=element.exercises
+  })
   return(
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const part = [
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+   {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
 
+  const getTotalExercises=()=>{
+    
+  }
   return (
     <div>
       <Header course= {course}/>
-      <Content part={[part1,part2,part3]}/>
+      <Content part={part}/>
 
-      <Total total = {part1.exercises+part2.exercises+part3.exercises}/>
+      <Total part = {part}/>
     </div>
   )
 }
