0a550e8c02ba484638ac4e4cd3ba22ca437837ef
Divyaswor Makai <makaidivya@gmail.com>
2020-06-25 17:25:20 +0545

    2.11 completed

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Numbers from './components/Numbers'
 import Phonebook from './components/PhoneBook'
 import AddForm from './components/AddForm'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '123456789' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
-  const [personToShow, setPersonToShow] = useState(persons)
+  const [personToShow, setPersonToShow] = useState([])
+
+  useEffect(()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response)=>{
+        console.log("Fetched data")
+        setPersons(response.data)
+        setPersonToShow(response.data)
+      })
+  },[])
 
   const filterNames =(val)=>{
     console.log("FilterN Names:",val)
