430abdeef521f64547f744b8fbc0fd2c09370a18
Divyaswor Makai <makaidivya@gmail.com>
2020-06-29 14:17:28 +0545

    3.13 and 3.14 completed

--- part2/src/services/PhoneServices.js
+++ part2/src/services/PhoneServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = 'https://fullstackopen-test-makai.herokuapp.com/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
+// const baseUrl = 'https://fullstackopen-test-makai.herokuapp.com/api/persons'
 
 const getAll =()=>{
     const req =axios.get(baseUrl)
--- part3/index.js
+++ part3/index.js
@@ -1,19 +1,102 @@
 const express = require('express')
 const morgan = require('morgan')
+const dotenv = require('dotenv').config()
 const cors = require('cors')
+const mongoose  = require('mongoose')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+const Contact = require('./models/contacts.model')
+const { response } = require('express')
+
+//middleware
 morgan.token('body', function getBody(req){
     return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :response-time - :body'))
+//database connection
+const url = process.env.MONGODB_URI
 
-let persons = [
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res)=> console.log("Connected to Database!!!"))
+  .catch((err)=>console.log("Could not connect to Database:\n"+err))
+
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    console.log("Finding people")
+    Contact.find({})
+    .then(result=>{
+        console.log("Phonebook:")
+        result.forEach(elem=>{
+            console.log(`${elem.name}: ${elem.number}`)
+        })
+        res.json(result)
+    })
+  })
+
+  app.get('/api/info',(req,res)=>{
+    const personInfo =`Persons has info for ${persons.length} people.`
+    const date = new Date()
+
+    res.send(`<div><p>${personInfo}</p><p>${date}</p></div>`)
+  })
+
+  app.get('/api/persons/:id',(req,res)=>{
+    const id = req.params.id
+    Contact.findById(id).then(contact=>{
+      if(contact.length===0){
+        res.status(200).send({msg:'No contact found'})
+      }
+      else{
+        res.json(contact)
+      }
+    })
+  })
+
+  app.delete('/api/persons/:id',(req,res)=>{
+      const id = req.params.id
+      Contact.findByIdAndDelete(id).then(result=>{
+        res.status(204).end()
+      })
+  })
+
+  app.post('/api/persons', (req,res)=>{
+      const body = req.body
+      console.log(body)
+
+      if(body.name ===undefined){
+        return response.status(400).json({error:'Name missing '})
+      }
+      if(body.name === '' || body.number===''){
+        res.status(501).send({
+            error:'Please fill the details of number and name'
+        })
+      }
+      else{
+        const newContact = new Contact({name:body.name,number:body.number})
+        newContact.save().then(savedNumber => res.json(savedNumber))
+      }
+  })
+    
+  const unknownEndPont = (req,res)=>{
+      res.status(404).send({error:'unknown endpoint'})
+  }
+
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+
+  let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -35,70 +118,3 @@ let persons = [
         "id": 4
       }
 ]
-
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
-
-  app.get('/api/info',(req,res)=>{
-    const personInfo =`Persons has info for ${persons.length} people.`
-    const date = new Date()
-
-    res.send(`<div><p>${personInfo}</p><p>${date}</p></div>`)
-  })
-
-  app.get('/api/persons/:id',(req,res)=>{
-      const id = req.params.id
-      
-    let personList = persons.find(person => person.id==id)
-      if(personList){
-          res.json(personList)
-      }
-      else{
-          res.status(404).end()
-      }
-  })
-
-  app.delete('/api/persons/:id',(req,res)=>{
-      const id = req.params.id
-      console.log(id)
-      let newPersons = persons.filter((person=>person.id!=id))
-      console.log("New List:",newPersons)
-      persons = newPersons
-      res.status(204).end()
-  })
-
-  app.post('/api/persons', (req,res)=>{
-      const body = req.body
-      console.log(body)
-      let objs = persons.filter(person=>person.name===body.name || person.number === body.number)
-      if(body.name === '' || body.number===''){
-        res.status(501).send({
-            error:'Please fill the details of number and name'
-        })
-      }
-      else if(objs.length>0){
-        res.status(501).send({
-            error:'The name/number already exists'
-        })
-      }
-      else{
-        const id = Math.floor(Math.random()*100000)
-        body.id = id
-        persons = persons.concat(body)
-        res.status(200).send(body)
-      }
-  })
-    
-  const unknownEndPont = (req,res)=>{
-      res.status(404).send({error:'unknown endpoint'})
-  }
-
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
