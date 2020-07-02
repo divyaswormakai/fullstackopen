ea755fb8c7a347c5f73b20fb6ee8d56beddaf465
Divyaswor Makai <makaidivya@gmail.com>
2020-07-02 16:13:23 +0545

    4.4 completed

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const likeCount = (blogs) => {
+  const reducer = (sum, blogs) => {
+    return sum + blogs.likes;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  likeCount,
 };
