b27899a014e5138cf97a53a95fc90cffba3533bb
Divyaswor Makai <makaidivya@gmail.com>
2020-07-04 22:05:50 +0545

    4.10 completed

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const logger = require('../utils/logger');
 const app = require('../app');
+const Blog = require('../models/blog.model');
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let tempData = new Blog(dummyData[0]);
+  await tempData.save();
+  let tempData2 = new Blog(dummyData[1]);
+  await tempData2.save();
+
+  logger.info('DATA hasbeen saved');
+});
 
 test('notes are returned as json', async () => {
   await api
@@ -12,21 +23,60 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
-// test('length of blogs be 2', async () => {
-//   const res = await api.get('/api/blogs');
-//   expect(res.body).toHaveLength(2);
-// });
-
-// test('the first blogs says Blog title 1', async () => {
-//   const response = await api.get('/api/blogs');
-
-//   expect(response.body[0].title).toBe('Blog title 1');
-// });
-
 test('toJSON has changed _id to id', async () => {
   const response = await api.get('/api/blogs');
   //check if the property is defined
   expect(response.body[0].id).toBeDefined();
+});
+
+describe('all about post method', () => {
+  test('POST method working', async () => {
+    const postedData = await api
+      .post('/api/blogs')
+      .send(postData)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(dummyData.length + 1);
+
+    expect(postData).toEqual(postData);
+  });
+});
+
+const dummyData = [
+  {
+    title: 'Blog title 1',
+    author: 'The original Makai',
+    url: 'http://www.divyaswormakai.com.np',
+    likes: 4,
+  },
+  {
+    title: 'Blog title 2',
+    author: 'The original Makai second',
+    url: 'http://www.divyaswormakai.com.np',
+    likes: 10,
+  },
+];
+
+const postData = {
+  title: 'Blog from post method',
+  author: 'Poster',
+  url: 'http://www.divyaswormakai.com.np',
+  likes: 0,
+};
+
+// test('length of blogs be 2', async () => {
+//   const res = await api.get('/api/blogs');
+//   expect(res.body).toHaveLength(dummyData.length);
+// });
+
+test('title checking', async () => {
+  const response = await api.get('/api/blogs');
+  let titles = response.body.map((data) => data.title);
+  //check if the property is defined
+  expect(titles).toContain('Blog title 2');
 });
 
 afterAll(() => {
