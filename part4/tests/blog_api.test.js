85e5bae185490cda26f19fc1b771dc0715312c75
Divyaswor Makai <makaidivya@gmail.com>
2020-07-04 13:42:21 +0545

    4.9 completed

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const logger = require('../utils/logger');
 const app = require('../app');
 
 const api = supertest(app);
@@ -9,6 +10,23 @@ test('notes are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+// test('length of blogs be 2', async () => {
+//   const res = await api.get('/api/blogs');
+//   expect(res.body).toHaveLength(2);
+// });
+
+// test('the first blogs says Blog title 1', async () => {
+//   const response = await api.get('/api/blogs');
+
+//   expect(response.body[0].title).toBe('Blog title 1');
+// });
+
+test('toJSON has changed _id to id', async () => {
+  const response = await api.get('/api/blogs');
+  //check if the property is defined
+  expect(response.body[0].id).toBeDefined();
 });
 
 afterAll(() => {
