a8b80966265e4e468d7de69c0a01a8bbc65b0725
Divyaswor Makai <makaidivya@gmail.com>
2020-08-02 11:52:25 +0545

    8.4 completed

--- Part8/index.js
+++ Part8/index.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -109,11 +109,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      let author = args.author;
+      return books.filter((book) => book.author === author);
+    },
     allAuthors: (root, args) => {
       return authors.map((author) => {
         let count = books.filter((book) => book.author === author.name).length;
-        console.log(count);
         return { name: author.name, bookCount: count };
       });
     },
