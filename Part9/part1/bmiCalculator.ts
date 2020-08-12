b505f5689bef2e44d91b9171f10f5af3f95cc8b9
Divyaswor Makai <makaidivya@gmail.com>
2020-08-12 12:56:28 +0545

    9.1 completed

---
+++ Part9/part1/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (mass: number, height: number) => {
+  const bmi = (mass * 100 * 100) / (height * height);
+  let result = '';
+  if (bmi < 20) {
+    result = 'UnderWeight';
+  } else if (bmi > 25) {
+    result = 'OverWeight';
+  } else {
+    result = 'Normal';
+  }
+  console.log(bmi);
+
+  return `${result} (${mass}, ${height})`;
+};
+
+console.log(calculateBmi(76, 165));
